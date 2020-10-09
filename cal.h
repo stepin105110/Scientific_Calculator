bdaac4abeb7e9e1a81200704200b2f32ae6fd9de
stepin105110 <69055251+stepin105110@users.noreply.github.com>
2020-10-10 00:45:09 +0530

    Add files via upload

---
+++ cal.h
@@ -0,0 +1,19 @@
+#ifndef __CAL_H__
+	#define __CAL_H__
+
+	float sine(float  x);
+    float cosine(float x);
+    float tangent(float x);
+    float sineh(float x);
+    float cosineh(float x);
+    float tangenth(float x);
+    float logten(float x);
+    float squareroot(float x);
+    float exponent(float x);
+    float power(float x,float y);
+    float add(float x,float y);
+    float subtract(float x,float y);
+    float multiply(float x,float y);
+    float divide(float x,float y);
+
+    #endif
